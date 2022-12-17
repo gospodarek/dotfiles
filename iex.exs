75fdf0cfa2affa1e841ba6f34b4ff97e98f7358c
Marsha Gospodarek <gospodarek@gmail.com>
2022-12-16 18:44:24 -0700

    first commit

---
+++ iex.exs
@@ -0,0 +1,19 @@
+Application.put_env(:elixir, :ansi_enabled, true)
+IEx.configure(
+  colors: [enabled: true],
+  default_prompt: [
+    "\e[G",    # ANSI CHA, move cursor to column 1
+    :magenta,
+    "%prefix", # IEx prompt variable
+    ">",       # plain string
+    :reset
+  ] |> IO.ANSI.format |> IO.chardata_to_string
+)
+
+defmodule IExHelpers do
+  def r do
+    IEx.Helpers.recompile
+  end
+end
+
+iex = IExHelpers
